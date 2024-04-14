758e7eb554a589716c0191fb369b7b727cfff670
unknown <daniel.winiarczyk18@gmail.com>
2024-04-15 01:19:16 +0200

    (feed:employee) Employes works fine, no delete option

--- server/services/reports/src/main/java/com/recykling/report/ReportApplication.java
+++ server/services/reports/src/main/java/com/recykling/report/ReportApplication.java
@@ -3,9 +3,11 @@ package com.recykling.report;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author WiniaR21
+ */
 @SpringBootApplication
 public class ReportApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ReportApplication.class, args);
 	}
