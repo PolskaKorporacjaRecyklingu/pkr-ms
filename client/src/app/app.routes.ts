5ab0d9328d1e2f99ca8f4d19adeb1c9867d11475
JohnnyProg <jangryta9@gmail.com>
2024-04-15 17:38:32 +0200

    (feat:tailwind): Initial configuration of tailwind

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import {LoginComponent} from "./core/auth/pages/login/login.component";
-import {RegisterComponent} from "./core/auth/pages/register/register.component";
+import {HomeComponent} from "./shared/pages/home/home.component";
 
 export const routes: Routes = [
   {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent}
+  {path: '', component: HomeComponent}
 ];
